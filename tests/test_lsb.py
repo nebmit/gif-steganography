0ab24c0cf81c79b22879f42bde238f75491c6bdf
nebmit <76664673+nebmit@users.noreply.github.com>
2025-04-08 19:57:51 +0200

    test: add large payload tests and add new test data

--- tests/test_lsb.py
+++ tests/test_lsb.py
@@ -4,39 +4,59 @@ import pytest
 
 from gif_steganography import SteganographyMethod, decode, encode
 
-GIF_FILES = ["dark.gif", "small.gif"]
+GIF_FILES = ["dark.gif"]
 INPUT_DIR = os.path.join(os.path.dirname(__file__), "data", "input")
 OUTPUT_PATH = os.path.join(os.path.dirname(__file__), "data", "output.gif")
 MODE = SteganographyMethod.LSB
 PASSPHRASE = "password"
 PAYLOAD = "Secret message."
+LARGE_PAYLOAD = """
+                                   / \\  //\\
+                    |\\___/|      /   \\//  \\\\\\
+                    /O  O  \\__  /    //  | \\ \\    
+                   /     /  \\/_/    //   |  \\  \\  
+                   @_^_@'/   \\/_   //    |   \\   \\ 
+                   //_^_/     \\/_ //     |    \\    \\
+                ( //) |        \\///      |     \\     \\
+              ( / /) _|_ /   )  //       |      \\     _\\
+            ( // /) '/,_ _ _/  ( ; -.    |    _ _\\.-~        .-~~~^-.
+          (( / / )) ,-{        _      `-.|.-~-.           .~         `.
+         (( // / ))  '/\\      /                 ~-. _ .-~      .-~^-.  \\
+         (( /// ))      `.   {            }                   /      \\  \\
+          ((/ ))     .----~-.\\        \\-'                 .~         \\  `. \\^-.
+                     ///.----..>        \\             _ -~             `.  ^-`  ^-_
+                       ///-._ _ _ _ _ _ _}^ - - - - ~                     ~-- ,.-~
+                                                                          /.-~
+"""
 
 
 @pytest.mark.parametrize("gif_file", GIF_FILES)
-def test_encode_decode(gif_file):
+@pytest.mark.parametrize("payload", [PAYLOAD, LARGE_PAYLOAD])
+def test_encode_decode(gif_file, payload):
     input_path = os.path.join(INPUT_DIR, gif_file)
-    encode(input_path, OUTPUT_PATH, PAYLOAD, mode=MODE)
+    encode(input_path, OUTPUT_PATH, payload, mode=MODE)
 
     message, _ = decode(OUTPUT_PATH, mode=MODE)
-    assert message == PAYLOAD
+    assert message == payload
 
     # Remove the output file
     os.remove(OUTPUT_PATH)
 
 
 @pytest.mark.parametrize("gif_file", GIF_FILES)
-def test_encode_decode_encrypted(gif_file):
+@pytest.mark.parametrize("payload", [PAYLOAD, LARGE_PAYLOAD])
+def test_encode_decode_encrypted(gif_file, payload):
     input_path = os.path.join(INPUT_DIR, gif_file)
     encode(
         input_path,
         OUTPUT_PATH,
-        PAYLOAD,
+        payload,
         passphrase=PASSPHRASE,
         mode=MODE,
     )
 
     message, _ = decode(OUTPUT_PATH, passphrase=PASSPHRASE, mode=MODE)
-    assert message == PAYLOAD
+    assert message == payload
 
     # Remove the output file
     os.remove(OUTPUT_PATH)
